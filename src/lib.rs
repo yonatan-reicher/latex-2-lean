d2ef168066ac301331bf1547684b46a31ad8006c
Yonatan Reicher <yony252525@gmail.com>
2025-05-29 11:07:28 +0300

    Add markdown parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,22 @@ mod precedence;
 mod formating;
 mod parsing;
 
+pub fn markdown_parse_options() -> markdown::ParseOptions {
+    use markdown::{Constructs, ParseOptions};
+    ParseOptions {
+        constructs: Constructs {
+            math_flow: true,
+            math_text: true,
+            ..ParseOptions::gfm().constructs
+        },
+        ..ParseOptions::gfm()
+    }
+}
+
 pub mod prelude {
     use super::*;
     pub use types::*;
+    pub use super::markdown_parse_options;
     pub use parsing::parse;
     pub use parsing::Error as ParseError;
     pub use formating::format;
