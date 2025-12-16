b6a7e93b5e95c90ecceef5d9ea9c155694ef1763
Jonathan Reicher <yony252525@gmail.com>
2025-12-16 16:53:46 +0200

    module root file

--- Latex2Lean.lean
+++ Latex2Lean.lean
@@ -1,3 +1,24 @@
--- This module serves as the root of the `Latex2Lean` library.
--- Import modules here that should be built as part of the library.
+-- Common things
 import Latex2Lean.Basic
+import Latex2Lean.Util
+-- Types
+import Latex2Lean.InlineMath
+import Latex2Lean.Span
+import Latex2Lean.Token
+import Latex2Lean.Formula
+import Latex2Lean.CategorizedFormula
+import Latex2Lean.Analysis
+import Latex2Lean.Csv
+import Latex2Lean.LeanCmd
+-- The different stages of our process
+import Latex2Lean.Input
+import Latex2Lean.Spanning
+import Latex2Lean.Lexing
+import Latex2Lean.Parsing
+import Latex2Lean.Categorizing
+import Latex2Lean.Analysing
+import Latex2Lean.Translating
+import Latex2Lean.Emitting
+-- Final API
+import Latex2Lean.Functions
+import Latex2Lean.Macros
