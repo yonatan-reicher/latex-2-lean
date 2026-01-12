259112c02452193f755d7c8e2652dfa2ad17b714
Jonathan Reicher <yony252525@gmail.com>
2026-01-12 13:18:18 +0200

    command token

--- Latex2Lean/Token.lean
+++ Latex2Lean/Token.lean
@@ -7,14 +7,17 @@ namespace Latex2Lean
 
 inductive Token.Kind
   | word (chars : Array Char)
+  /-- A word, but starts with a '\' -/
+  | command (chars : Array Char)
   | number (number : Nat)
-  deriving DecidableEq, Repr
+  deriving DecidableEq, Repr, Inhabited
 
 
 structure Token where
   kind : Token.Kind
   /-- The text range of the token in the original text -/
   range : Range
+  deriving Inhabited
 
 
 /-- This is like a version of `Token.Kind.word` but that's meant to be used as a
@@ -22,3 +25,10 @@ structure Token where
 @[match_pattern]
 def Token.Kind.word' : String → Token.Kind
   | s => .word s.toList.toArray
+
+
+/-- This is like a version of `Token.Kind.command` but that's meant to be used as a
+  nicer match pattern, because it has a string argument! -/
+@[match_pattern]
+def Token.Kind.command' : String → Token.Kind
+  | s => .command s.toList.toArray
