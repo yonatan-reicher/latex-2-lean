df975d0ffa3f14ccfe72a557e876a170cda40dec
Jonathan Reicher <yony252525@gmail.com>
2026-01-13 23:18:40 +0200

    Pos le, min and max

--- Latex2Lean/Pos.lean
+++ Latex2Lean/Pos.lean
@@ -30,4 +30,20 @@ def advance : Char → Pos → Pos
   | _, { row, col, .. } => { row := row, col := col + 1 }
 
 
+instance : LE Pos where
+  le p1 p2 :=
+    p1.row <= p2.row ∨ (p1.row = p2.row ∧ p1.col <= p2.col)
+instance : DecidableRel (@LE.le Pos _) := by
+  intros p1 p2
+  simp only [(· ≤ · )]
+  simp only [Nat.le_eq]
+  infer_instance
+
+instance : Min Pos where min p1 p2 := if p1 ≤ p2 then p1 else p2
+instance : Max Pos where max p1 p2 := if p1 ≤ p2 then p2 else p1
+
+
+#guard min (Pos.mk 1 2) (Pos.mk 2 1) = Pos.mk 1 2
+
+
 end Pos
