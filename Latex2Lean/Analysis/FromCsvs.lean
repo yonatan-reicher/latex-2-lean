5d0de759e93d599a2db00eb4029158c47e8af77e
Jonathan Reicher <yony252525@gmail.com>
2026-02-16 08:51:33 -0500

    fix

--- Latex2Lean/Analysis/FromCsvs.lean
+++ Latex2Lean/Analysis/FromCsvs.lean
@@ -27,7 +27,7 @@ code.
 def AnalysisResult.fromCsvs (data : List Csv)
 : Except String Analysis := do
   let csvNames :=
-    data.map (·.fileName)
+    data.map (·.fileName.toString)
     -- WHY would someone name this INTERCALATE??
     |> String.intercalate ", "
   let isFiniteSetCsvName := "expr_is_finite_set.csv"
@@ -35,10 +35,10 @@ def AnalysisResult.fromCsvs (data : List Csv)
 
   -- Find the Csvs!
   let some isFiniteSetCsv :=
-    data.find? (·.fileName == isFiniteSetCsvName)
+    data.find? (·.fileName.toString == isFiniteSetCsvName)
     | throw s!"No CSV file named '{isFiniteSetCsvName}'. Found only '{csvNames}'"
   let some mustBeFiniteSetCsv :=
-    data.find? (·.fileName == mustBeFiniteSetCsvName)
+    data.find? (·.fileName.toString == mustBeFiniteSetCsvName)
     | throw s!"No CSV file named '{mustBeFiniteSetCsvName}'. Found only '{csvNames}'"
 
   return {
