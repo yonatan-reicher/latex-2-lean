86b4bfd0376e5f5b36eb1516a4a7f52ac965ba12
Jonathan Reicher <yony252525@gmail.com>
2026-02-18 06:47:51 -0500

    WIP

--- Latex2Lean/Translating.lean
+++ Latex2Lean/Translating.lean
@@ -3,6 +3,7 @@ import Latex2Lean.Analysis
 import Latex2Lean.LeanCmd
 
 import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Card
 import Mathlib.Data.Set.Basic
 
 import Lean
@@ -15,11 +16,36 @@ code.
 
 namespace Latex2Lean
 
+open Lean (
+  Expr
+  CoreM
+  MetaM
+  Syntax
+  TSyntax
+  Term
+  mkIdent
+  mkNatLit
+)
+open Lean.Meta
+open Lean.Elab.Term (
+  TermElabM
+  exprToSyntax
+  elabType
+  elabTermEnsuringType
+)
+
 
 private abbrev CF := CategorizedFormula
 private abbrev F := Formula
 private abbrev Name := Array Char
-private abbrev M := ReaderT Analysis Lean.Elab.TermElabM
+private abbrev M := ReaderT Analysis TermElabM
+
+
+instance : MonadLift CoreM M where monadLift := fun x _ => x
+instance : MonadLift MetaM M where monadLift := fun x _ => x
+
+
+-- Helpers
 
 
 private def isFiniteSet (name : Name) : M Bool := do
@@ -31,19 +57,124 @@ private def mustBeFiniteSet (name : Name) : M Bool := do
   return mustBeFiniteSet.contains ⟨name, []⟩
 
 
-private def asFinset (f : F) : M Lean.Expr := do
-  let elementMVar ← Lean.Meta.mkFreshTypeMVar
-  let elementTerm ← Lean.Elab.Term.exprToSyntax elementMVar
-  let finset ← Lean.Elab.Term.elabType (← ``(Finset $elementTerm))
-  let stx <- match f with
+private def varToIdent (name : Name) : Lean.Ident := mkIdent (.mkSimple name)
+
+private def varToTerm (name : Name) : M Term := ``($(varToIdent name))
+
+private def varToExpr (name : Name) : M Expr := do
+  elabTermEnsuringType (← varToTerm name) none
+
+
+private def empty (t : Option Expr) : M Expr := do
+  elabTermEnsuringType (← ``(∅)) t
+
+
+mutual
+
+
+private partial def asNumber : F → M Expr
+  | .var name .. => varToExpr name
+  | .number n .. => return mkNatLit n
+  | .abs inner .. => do
+    -- TODO: What if inner is actually a number?
+    let innerExpr ← asFinset inner
+    mkAppM ``Finset.card #[innerExpr]
+  | .binOp left op right .. => do
+    let leftExpr ← asNumber left
+    let rightExpr ← asNumber right
+    let f ← match op with
+      | .plus => pure ``HAdd.hAdd
+      | .star => pure ``HMul.hMul
+      | _ => throwError s!"unsupported binary operator for translation to number: {repr op}"
+    mkAppM f #[leftExpr, rightExpr]
+  | f => throwError s!"unsupported formula for translation to number: {repr f}"
+
+
+private partial def asFinset (f : F) : M Expr := do
+  let elementMVar ← mkFreshTypeMVar
+  let elementTerm ← exprToSyntax elementMVar
+  let finset ← elabType (← ``(Finset $elementTerm))
+  let stx ← toStx f
+  elabTermEnsuringType stx finset
+where
+  toStx
     | .emptySet .. => ``(Finset.empty)
-    | .var name .. => pure <| Lean.mkIdent (.mkSimple name)
-    | _ => panic "a"
-  Lean.Elab.Term.elabTermEnsuringType stx finset
+    | .var name .. => return mkIdent (.mkSimple name)
+    | .number n .. => throwError s!"cannot translate number {n} into a finset"
+    | .abs .. => do
+      -- mkAppM ``Finset.card #[← asFinset inner]
+      throwError s!"cannot translate absolute value into a finset"
+    -- | .binOp (left : Formula) (op : BinOp) (right : Formula)
+    | .simpleSet elements .. => do
+      let elements ← elements.mapM asWhatever
+      let array ← mkListLit (←mkFreshTypeMVar) elements.toList
+      let ret ← mkAppM ``List.toFinset #[array]
+      exprToSyntax ret
+    -- | .mapSet (lhs : Formula) (binders : Array Formula.Binder) (range : Range)
+    -- | .tuple (elements : Array Formula) (range : Range)
+    | f => throwError s!"unsupported formula for translation to finset: {repr f}"
 
 
-private def asSet (f : F) : M Lean.Expr :=
-  panic! "asSet"
+private partial def asSet : F → M Expr
+  | .emptySet .. => do empty $ some $ ← setType
+  | .var name .. => varToExpr name
+  | .number n .. => throwError s!"cannot translate number {n} into a set"
+  | .abs .. => throwError s!"cannot translate absolute value into a set"
+  | .binOp left op right .. => do
+    let leftExpr ← asSet left
+    let rightExpr ← asSet right
+    let f ← match op with
+      | .cap => pure ``Set.inter
+      | .cup => pure ``Set.union
+      | _ => throwError s!"unsupported binary operator for translation to set: {repr op}"
+    mkAppM f #[leftExpr, rightExpr]
+  | .simpleSet elements range => do
+    if elements.isEmpty
+    then asSet (.emptySet range)
+    else
+      let elements ← elements.mapM (asWhatever · >>= liftM ∘ exprToSyntax)
+      let separated : Syntax.TSepArray `term "," := .ofElems elements
+      let stx ← ``(({ $separated:term,* } : Set _))
+      elabTermEnsuringType stx (some (← setType))
+  | f => throwError s!"unsupported formula for translation to set: {repr f}"
+where
+  setType : M Expr := do
+    let elementType ← mkFreshTypeMVar
+    let elementStx ← exprToSyntax elementType
+    elabType (← ``(Set $elementStx))
+
+
+private partial def asTuple (f : F) : M Expr :=
+  panic! "asTuple"
+
+
+private partial def asWhatever (f : F) : M Expr :=
+  match f with
+  | .emptySet .. => asSet f
+  | .var name .. => varToExpr name
+  | .number .. => asNumber f
+  | .abs .. => asNumber f
+  | .binOp left op right => do
+    let left ← asWhatever left
+    let left ← exprToSyntax left
+    let right ← asWhatever right
+    let right ← exprToSyntax right
+    let stx ← match op with
+      | .plus => ``($left + $right)
+      | .minus => ``($left - $right)
+      | .star => ``($left * $right)
+      | .slash => ``($left / $right)
+      | .cap => ``($left ∩ $right)
+      | .cup => ``($left ∪ $right)
+      | .eq => ``($left = $right)
+      | .in_ => ``($left ∈ $right)
+    elabTermEnsuringType stx none
+  | .simpleSet .. => asSet f
+  | .mapSet .. => asSet f
+  | .tuple .. => asTuple f
+
+
+end
 
 
 /-- Translate a definition. Needs to decide the type to translate into. -/
@@ -55,7 +186,7 @@ private def definition (name : Name) (f : F) : M LeanCmd := do
     then return .def_ leanName (← asFinset f)
     else return panic!" tarstastarstat!!!!!!!! "
   else
-    return .def_ leanName (← asSet f)
+    return .def_ leanName (← asWhatever f)
 
 
 /-- Translate an axiom. Needs to translate into a proposition. -/
