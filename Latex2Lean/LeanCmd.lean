12f3a4f44fd8e35829f9c0e3921dd6d8edcd297d
Jonathan Reicher <yony252525@gmail.com>
2026-02-19 08:41:50 -0500

    add LeanCmd.prettyPrint

--- Latex2Lean/LeanCmd.lean
+++ Latex2Lean/LeanCmd.lean
@@ -1,10 +1,20 @@
 import Lean.Expr
+import Lean.PrettyPrinter
 
 
 namespace Latex2Lean
+
+open Lean.Meta
 
 
 inductive LeanCmd where
   | def_ (name : Lean.Name) (e : Lean.Expr)
   | axiom_ (e : Lean.Expr)
   deriving Inhabited, Repr, BEq
+
+
+def LeanCmd.prettyPrint : LeanCmd → Lean.MetaM String
+  | .def_ name e => do
+    let typ ← inferType e
+    return s!"def {name} : {typ} := {← ppExpr e}"
+  | .axiom_ e => return s!"axiom {e}"
