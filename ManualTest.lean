5f281e2fd2d0e0a5f8c31acd79465690e60973fe
Jonathan Reicher <yony252525@gmail.com>
2026-02-22 04:10:59 -0500

    update ManualTest.lean

--- ManualTest.lean
+++ ManualTest.lean
@@ -9,3 +9,17 @@ define_latex r"$X = \set{ (1, 2) }$"
 
 def y := 2
 define_latex file "proof-adjusted.md"
+
+-- #eval defineLatex "This is some text with inline math $x = 2$."
+define_latex verbose r"
+
+This is some text with inline math $x = \set{ 1, 2 }$, $\abs x$.
+$$
+  Y = \set{ \set{ 1, 2 } }
+$$
+
+"
+#print x
+#print Y
+
+define_latex "This is some text with inline math $(x + y) = z$."
