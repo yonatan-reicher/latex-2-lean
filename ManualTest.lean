ede08ba666c1dd081f0ccc1ba407c5f01ff45557
Jonathan Reicher <yony252525@gmail.com>
2026-02-23 08:54:57 -0500

    update ManualTest.lean

--- ManualTest.lean
+++ ManualTest.lean
@@ -1,26 +1,12 @@
 import Latex2Lean
 
 
-define_latex r"ar"
-
-define_latex r"$X = \set{ (1, 2) }$"
-#print X
-
 def y := 2
-define_latex file "proof-adjusted.md"
-#print D
-#reduce D
+define_latex file verbose "proof-adjusted.md"
 
--- #eval defineLatex "This is some text with inline math $x = 2$."
 define_latex verbose r"
-
-This is some text with inline math $x = \set{ 1, 2 }$, $\abs x$.
 $$
-  Y = \set{ \set{ 1, 2 } }
+X = \{ x + 1 \mid x \in \{1, 3\} \}
 $$
-
 "
-#print x
-#print Y
-
-define_latex "This is some text with inline math $(x + y) = z$."
+#print X
