efa6f35ce1950cf86bb6da1eacc49393c335714d
Jonathan Reicher <yony252525@gmail.com>
2026-03-05 07:45:50 -0500

    reading ManualTest.lean to the next mission

--- ManualTest.lean
+++ ManualTest.lean
@@ -3,18 +3,14 @@ import Mathlib
 
 
 axiom D {α} : Set α
-define_latex verbose r"X \subseteq \emptyset"
 define_latex file verbose "proof-adjusted.md"
 
+
 define_latex verbose r"
-$$
-X = \{ x + 1 \mid x \in \{1, 3\} \}
-$$
-$$
-Y = \mset{ 1, 2, 5, 2 }
-$$
-hello! $a = \abs X$
+
+Let $x = \sum \mset{ 1 }$
+
+
 "
-#print X
-#print Y
-#print a
+#print x
+#guard x = 1
