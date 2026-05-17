ca3aabcb786d428ba9c19ccc09bdd5d27a994755
Jonathan Reicher <yony252525@gmail.com>
2026-05-18 00:19:45 +0300

    comments

--- zfc-abstractions/building_blocks/base.py
+++ zfc-abstractions/building_blocks/base.py
@@ -1,10 +1,13 @@
 from z3 import AstRef, BoolRef, ModelRef
 
 class EvalAt:
+    # TODO: Shouldn't this function be top level, and check via isinstance what
+    # to do? Or instead, we could monkey-patch the object class.
     def eval(self, m: ModelRef) -> Any: return m.eval(self, model_completion=True)
     def __matmul__(self, m: ModelRef): return self.eval(m)
 
 class IGuarded:
+    # TODO: See above todo.
     def guards(self) -> list[BoolRef]: return []
 
     @classmethod
