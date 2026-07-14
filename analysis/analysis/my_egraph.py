a0b429d5a41bc1d35ec2a7d6278ca5f9af6cd322
Jonathan Reicher <yony252525@gmail.com>
2026-07-14 14:09:58 +0300

    add my_egraph.py

---
+++ analysis/analysis/my_egraph.py
@@ -0,0 +1,7 @@
+from egglog import EGraph
+
+class MyEGraph(EGraph):
+    """ Currently, exactly the same as the regular e-graph. Later, will be
+    extended with convieniences. """
+    def __init__(self, save_egglog_string=True):
+        super().__init__(save_egglog_string=save_egglog_string)
