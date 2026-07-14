ee622cff4b79962851f3260e9253bb75a3f43d13
Jonathan Reicher <yony252525@gmail.com>
2026-07-14 18:06:21 +0300

    fix typo, ignore false type error

--- analysis/analysis/my_egraph.py
+++ analysis/analysis/my_egraph.py
@@ -2,6 +2,6 @@ from egglog import EGraph
 
 class MyEGraph(EGraph):
     """ Currently, exactly the same as the regular e-graph. Later, will be
-    extended with convieniences. """
+    extended with conveniences. """
     def __init__(self, save_egglog_string=True):
-        super().__init__(save_egglog_string=save_egglog_string)
+        super().__init__(save_egglog_string=save_egglog_string) # type: ignore
