1487551a2459a4b94df072fbce834ae31d42b576
Jonathan Reicher <yony252525@gmail.com>
2026-07-15 08:27:04 +0300

    expand utils.py

--- analysis/analysis/utils.py
+++ analysis/analysis/utils.py
@@ -1,10 +1,19 @@
 """ What do you think this module is about? """
 
 from sys import exit
+import traceback as tb
 
 def bad_exit(reason: str | None = None):
     if reason is not None:
+        stack = tb.extract_stack()
+        stack.pop()
         print(f"Error: {reason}")
-    exit(1)
+        tb.print_list(stack)
+        print(f"Error: {reason}")
+    return exit(1)
 
 def raise_(e: Exception): raise e
+
+def sorry():
+    """ mark unimplemented code """
+    return bad_exit('this part of the code has been left unimplemented')
