9c20b817c234cf289ad324b4eee0d79782f72727
Jonathan Reicher <yony252525@gmail.com>
2026-08-04 20:16:05 +0300

    whitespace

--- analysis/analysis/rules.py
+++ analysis/analysis/rules.py
@@ -5,8 +5,7 @@ def is_set_of_is_finite_set(ast: Ast):
     yield rule(ast.is_finite()).then(ast.is_set())
 
 def is_finite(ast: Ast, elements: Vec[i64]):
-    yield rule(eq(ast.kind()).to(AstKind.set(elements))).then(
-        ast.is_finite())
+    yield rule(eq(ast.kind()).to(AstKind.set(elements))).then(ast.is_finite())
 
 all = [
     *ast_rules,
